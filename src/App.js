20878fa5d75e3a51e72cc6b8e7b2d5f85bdf0441
Ravi Ketavath <raviketavath@Ravis-MacBook-Pro.local>
2024-05-07 09:14:47 -0400

    second msg to commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>This is Heading</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            this my first react app
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h2>React Application</h2>
-      </header>
+      <h1>Hello world iam ravi rathod from hyderabad</h1>
       <h2>Hello World</h2>
     </div>
   );
